b9d986862e3c72782db4a3fe915a066edd6c1b74
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-07-10 12:26:20 +0900

    ✨feat: コンストラクタの改善

--- src/map_changer.cpp
+++ src/map_changer.cpp
@@ -3,9 +3,19 @@
 namespace map_changer2
 {
 
-MapChanger::MapChanger()
+MapChanger::MapChanger(const rclcpp::NodeOptions& options) : rclcpp::Node("map_changer", options)
 {
+    initial_pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/initialpose", 10);
+
+    this->get_parameter("map_name", map_name_);
 }
+
+bool MapChanger::change_map_callback(const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
+                                        std::shared_ptr<std_srvs::srv::SetBool::Response> response)
+{
+    change_flag_ = true;
+                                        }
+
 
 MapChanger::~MapChanger()
 {
