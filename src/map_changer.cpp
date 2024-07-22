676f3b722e7f5ead8c802f218868281c1daf619d
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-07-22 17:34:15 +0900

    ✨feat: シェアディレクトリ追加

--- src/map_changer.cpp
+++ src/map_changer.cpp
@@ -1,4 +1,5 @@
 #include "map_changer2/map_changer.hpp"
+#include <ament_index_cpp/get_package_share_directory.hpp>
 
 namespace map_changer2
 {
@@ -11,8 +12,8 @@ MapChanger::MapChanger(const rclcpp::NodeOptions& options) : rclcpp::Node("map_c
 
     initial_pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>("/initialpose", 10);
 
-    this->get_parameter("filename", filename_);
-    
+    // this->get_parameter("filename", filename_);
+    filename_ = ament_index_cpp::get_package_share_directory("map_changer2")+"/config/"+"map_list.yaml";
 }
 
 bool MapChanger::change_map_callback(const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
